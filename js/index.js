a707773a7c0bcffd7d167c0bb8ae005282befee8
thedarkhunterasif <iasif619@gmail.com>
2022-08-10 22:42:56 +0600

    second commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function validPin() {
     const pin = Math.round(Math.random()*10000);
     const sum = pin + '';
-   
+
     if(sum.length == 4){
         console.log(pin);
        return pin;
@@ -9,13 +9,27 @@ function validPin() {
     //if digit length is not 4, return the function again
     else {
         return validPin();
-    }
-    
+    }  
 }
 
 
 document.getElementById('pin-generate').addEventListener('click', function(){
     const pin =validPin();
-    document.getElementById('input-pin').value = pin;
+    document.getElementById('input-pin').value = pin; 
 });
 
+                //get keyboaboard value by press
+document.getElementById('all-keyboard').addEventListener("click", function(event){
+    var keyValue = event.target.innerText;
+    var input =document.getElementById('input-key');
+    if(isNaN(keyValue)){
+        if(keyValue == 'C'){
+            input.value = '';
+        }
+    }
+    else {
+        input.value = input.value +  keyValue;
+        // var sum = inputValue +  keyValue;
+
+    }
+});

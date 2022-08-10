1a2915bdd3a3f3973013cc5ef1d6c6e399df154a
thedarkhunterasif <iasif619@gmail.com>
2022-08-11 01:21:24 +0600

    fourth commits

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 function validPin() {
     const pin = Math.round(Math.random()*10000);
     const sum = pin + '';
-
     if(sum.length == 4){
         console.log(pin);
        return pin;
@@ -12,25 +11,71 @@ function validPin() {
     }  
 }
 
-
+var pin;
+var inputPin = document.getElementById('input-pin');
 document.getElementById('pin-generate').addEventListener('click', function(){
     const pin =validPin();
-    document.getElementById('input-pin').value = pin; 
+    inputPin.value = pin; 
+    input.value = '';
+    pinMatchText.style.display = 'none';
+        pinNotMatchText.style.display = 'none';
+    return pin;
+    
 });
 
                 //get keyboaboard value by press
+ var sum;       
+ var input =document.getElementById('input-key');        
 document.getElementById('all-keyboard').addEventListener("click", function(event){
     var keyValue = event.target.innerText;
-    var input =document.getElementById('input-key');
+    
     if(isNaN(keyValue)){
         if(keyValue == 'C'){
             input.value = '';
         }
     }
     else {
-        input.value = input.value +  keyValue;
+       sum = input.value +  keyValue;
+        input.value = sum;
         // var sum = inputValue +  keyValue;
+        
+
+    }
+    return sum;
+});
+var tryNum = 0;
+var actionLeft = document.getElementById('action-left');
+var parentActionLeft = document.getElementById('parent-action-left');
+var pinMatchText = document.getElementById('pin-match-text');
+var pinNotMatchText = document.getElementById('pin-not-match-text');
+
+document.getElementById('submit-btn').addEventListener("click", function(){
+    tryNum = tryNum+1;
+    var actionLeftValue = parseFloat(actionLeft.innerText);
+    actionLeftValue = actionLeftValue - 1;
+    actionLeft.innerText = actionLeftValue;
+    if(input.value == inputPin.value){
+        console.log('success');
+        parentActionLeft.remove();
+        pinMatchText.style.display = 'block';
+        pinNotMatchText.style.display = 'none';
+        // window.location.href = "https://www.youtube.com/watch?v=ODIHw8NvhEE";
+       
+    }
+    else {
+        console.log('something is wrong');
+        input.value = '';
+        pinNotMatchText.style.display = 'block';
+        if(tryNum == 3 && actionLeftValue == 0){
+            console.log('try is over');
+            inputPin.value = '';
+            pinNotMatchText.style.display = 'none';
+           
+            // window.location.href = "http://127.0.0.1:5500/index.html";
+        }
 
     }
 });
-//third commits
+   
+
+            //third commits
